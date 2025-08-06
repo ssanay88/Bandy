85e07a57ac3a4a66e93e092e694edb53fa2576c6
yangsuhyuck <pplloo748@gmail.com>
2025-08-06 17:48:12 +0900

    feature / Create Band 이후 MyBand 탭이로 이동 시 처리 추가

--- feature/src/main/java/suhyeok/yang/feature/ui/myband/MyBandViewModel.kt
+++ feature/src/main/java/suhyeok/yang/feature/ui/myband/MyBandViewModel.kt
@@ -5,14 +5,17 @@ import androidx.lifecycle.viewModelScope
 import com.yang.business.common.DataResourceResult
 import com.yang.business.model.Band
 import com.yang.business.usecase.band.BandUseCases
+import com.yang.business.usecase.usersession.UserSessionUseCases
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class MyBandViewModel(
-    val bandUseCases: BandUseCases
+    val bandUseCases: BandUseCases,
+    val userSessionUseCase: UserSessionUseCases
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(MyBandUiState())
     val uiState = _uiState.asStateFlow()
@@ -22,40 +25,38 @@ class MyBandViewModel(
     }
 
     fun checkHasBand() {
-        // TODO datastore을 통해 회원 정보 불러와서 넣기
         viewModelScope.launch {
-            bandUseCases.readBand("band_004").onEach { result ->
-                _uiState.update {
-                    when (result) {
-                        is DataResourceResult.Success -> {
-                            it.copy(
-                                hasBand = true,
-                                myBand = result.data,
-                                isMyBandLoading = false
-                            )
-                        }
+            userSessionUseCase.getUserSession().collectLatest { userSession ->
+                bandUseCases.readBand(userSession.bandId).collectLatest { result ->
+                    _uiState.update {
+                        when (result) {
+                            is DataResourceResult.Success -> {
+                                it.copy(
+                                    hasBand = true,
+                                    myBand = result.data,
+                                    isMyBandLoading = false
+                                )
+                            }
 
-                        is DataResourceResult.Failure -> {
-                            it.copy(
-                                myBandErrorMessage = result.exception.message,
-                                isMyBandLoading = false
-                            )
-                        }
+                            is DataResourceResult.Failure -> {
+                                it.copy(
+                                    myBandErrorMessage = result.exception.message,
+                                    isMyBandLoading = false
+                                )
+                            }
 
-                        is DataResourceResult.Loading -> {
-                            it.copy(isMyBandLoading = true)
-                        }
+                            is DataResourceResult.Loading -> {
+                                it.copy(isMyBandLoading = true)
+                            }
 
-                        else -> {
-                            it
-                        }
+                            else -> {
+                                it
+                            }
 
+                        }
                     }
                 }
             }
-
-
         }
-
     }
 }
